68da96892e80f9aa8a77733199405df733101e03
Tomasz Skora <tskora93@gmail.com>
2018-02-03 12:15:43 +0100

    Implementation finished.

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,12 +1,14 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.lang.IllegalArgumentException;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Collection<Product> products = new ArrayList<Product>();
 	private BigDecimal subtotal = new BigDecimal("0.00");
 	private BigDecimal tax = new BigDecimal("0.00");
 	private BigDecimal total = new BigDecimal("0.00");
@@ -18,7 +20,10 @@ public class Invoice {
 		this.tax = this.tax.add(product.getPriceWithTax().subtract(product.getPrice()));
 	}
 
-	public void addProduct(Product product, Integer quantity) {
+	public void addProduct(Product product, Integer quantity) throws IllegalArgumentException {
+		if (quantity <= 0) {
+			throw new IllegalArgumentException();
+		}
 		for (int i=0; i<quantity; i++) {
 			this.addProduct(product);
 		}
