6f48ab6e002669e05354f1dc0a4092a5b906dd74
Tomasz Skora <tskora93@gmail.com>
2018-02-03 11:46:03 +0100

    Invoice testing in progress.

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -7,24 +7,32 @@ import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
 	private Collection<Product> products;
+	private BigDecimal subtotal = new BigDecimal("0.00");
+	private BigDecimal tax = new BigDecimal("0.00");
+	private BigDecimal total = new BigDecimal("0.00");
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.products.add(product);
+		this.subtotal = this.subtotal.add(product.getPrice());
+		this.total = this.total.add(product.getPriceWithTax());
+		this.tax = this.tax.add(product.getPriceWithTax().subtract(product.getPrice()));
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		for (int i=0; i<quantity; i++) {
+			this.addProduct(product);
+		}
 	}
 
 	public BigDecimal getSubtotal() {
-		return null;
+		return this.subtotal;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		return this.tax;
 	}
 
 	public BigDecimal getTotal() {
-		return null;
+		return this.total;
 	}
 }
